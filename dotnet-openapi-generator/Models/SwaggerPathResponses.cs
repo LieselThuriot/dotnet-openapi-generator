d326f869b3acacbc7dd85acda9e9a851e13695a1
Liesel Thuriot <git@thuriot.be>
2026-01-07 14:20:22 +0100

    Resolve order issue for returntype resolving

--- dotnet-openapi-generator/Models/SwaggerPathResponses.cs
+++ dotnet-openapi-generator/Models/SwaggerPathResponses.cs
@@ -2,5 +2,25 @@
 
 internal sealed class SwaggerPathResponses : Dictionary<string, SwaggerPathRequestBody>
 {
-    public string ResolveType() => Values.FirstOrDefault()?.ResolveType() ?? "";
+    public string ResolveType()
+    {
+        // Since we don't support responses based on status codes yet, we will just return the 200 response type if it exists.
+        if (TryGetValue("200", out SwaggerPathRequestBody? response))
+        {
+            return response.ResolveType();
+        }
+
+        // Or 201
+        if (TryGetValue("201", out response))
+        {
+            return response.ResolveType();
+        }
+
+        // Otherwise, order by status code and return the first successful response type we find, or just the first if none exist.
+        return this.OrderBy(x => x.Key.FirstOrDefault() is '2' ? 0 : 1)
+                   .ThenBy(x => x.Key)
+                   .Select(x => x.Value)
+                   .FirstOrDefault()
+                  ?.ResolveType() ?? "";
+    }
 }
